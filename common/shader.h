cf4dc711d0204d0c112d94481e66fbbc5369df82
Spyridonidis98 <79518257+Spyridonidis98@users.noreply.github.com>
2022-10-13 14:56:57 +0300

    Add files via upload

---
+++ common/shader.h
@@ -0,0 +1,10 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+GLuint loadShaders(const char* vertexFilePath,
+                   const char* fragmentFilePath,
+                   const char* geometryFilePath = nullptr);
+
+
+
+#endif
